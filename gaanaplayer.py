178e57864dcd4ac88b3aedf4dce31b00c7cc3b93
h3itik <tayadehritik@gmail.com>
2017-02-01 17:06:55 +0530

    Update gaanaplayer.py

--- gaanaplayer.py
+++ gaanaplayer.py
@@ -4,7 +4,7 @@ import time
 total_breaks = 3
 break_count = 0
 
-print("This program started one "+time.ctime())
+print("This program started on "+time.ctime())
 while (break_count < total_breaks):
     time.sleep(1800)
     webbrowser.open("http://gaana.com/song/let-me-love-you-415")
